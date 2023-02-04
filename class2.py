830b0382e0f87da6db433c2ffd5050daf16d6367
vishwaksaikumar <123284677+vishwaksaikumar@users.noreply.github.com>
2023-02-04 15:51:02 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,288 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+a=123
+print(type(a))
+
+
+# In[2]:
+
+
+a="123"
+print(type(a))
+
+
+# In[5]:
+
+
+a=123
+print(isinstance(a,str))
+
+
+# In[6]:
+
+
+a=123
+print(float(a))
+
+
+# In[7]:
+
+
+a=123
+print(hex(a))
+
+
+# In[8]:
+
+
+a=123
+print(oct(a))
+
+
+# In[10]:
+
+
+a="123"
+print(int(a))
+
+
+# In[11]:
+
+
+a="test"
+print(str(a))
+
+
+# In[12]:
+
+
+a="test"
+print(char(a))
+
+
+# In[13]:
+
+
+a=123
+
+
+# In[14]:
+
+
+a=123
+print(type(a))
+
+
+# In[16]:
+
+
+a=123
+print(isinstance(a,int))
+
+
+# In[17]:
+
+
+a=123
+print(float(a))
+
+
+# In[18]:
+
+
+a=123
+print(hex(a))
+
+
+# In[19]:
+
+
+a=123
+print(oct(a))
+
+
+# In[20]:
+
+
+a="123"
+print(int(a))
+
+
+# In[21]:
+
+
+a="test"
+print(isdigit(a))
+
+
+# In[23]:
+
+
+a.isdigit()
+
+
+# In[24]:
+
+
+a='123'
+print(a.isdigit())
+
+
+# In[25]:
+
+
+a=input()
+print(a)
+print(type(a))
+
+
+# In[26]:
+
+
+a=input()
+print(max(a))
+print(min(a))
+print(sorted(a))
+print(a.upper())
+
+
+# In[27]:
+
+
+a=input()
+print(a.index())
+print(len(a))
+
+
+# In[28]:
+
+
+a=input()
+print(a.index(w))
+print(len(a))
+
+
+# In[29]:
+
+
+a=vishwak
+print(max(a))
+print(a.index(h))
+
+
+# In[30]:
+
+
+a="vishwak"
+print(a.index())
+
+
+# In[31]:
+
+
+a=input()
+print(a)
+print(type(a))
+
+
+# In[32]:
+
+
+a=input()
+print (max(a))
+print(min(a))
+print(sorted(a))
+
+
+# In[33]:
+
+
+a=input()
+print (alphabatic(a))
+print(numeric(a))
+
+
+# In[34]:
+
+
+a="test"
+print(index("a"))
+
+
+# In[35]:
+
+
+a="test"
+print(a.index("a"))
+
+
+# In[36]:
+
+
+a="test"
+print(a.index("s"))
+
+
+# In[37]:
+
+
+a=input()
+print(max(a))
+print(min(a))
+print(len(a))
+print(sorted(a))
+print(a.index("h"))
+
+
+# In[38]:
+
+
+a="test"
+print(a.isalpha()) ##it was alphabetic..
+
+
+# In[39]:
+
+
+print(a.isalnum()) ##it may be alphabetic or numerics..
+
+
+# In[40]:
+
+
+print(a.isdigit()) ##whether it is digit means it will be true..
+
+
+# In[44]:
+
+
+a=["vishwak","22","chennai"]
+print(type(a))
+print(isinstance(a,int))
+
+
+# In[46]:
+
+
+a=[100,20,30,56,78]
+print(max(a))
+print(min(a))
+print(sorted(a))
+
+
+# In[48]:
+
+
+a=[100,20,30,56,78,-21]
+print(min(a))
+
+
+# In[ ]:
+
+
+
+
